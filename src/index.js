64dd02baac016e9aa8e45f0ab2806a5ea38cab05
6AHK0MAT <makhmutov.va@mail.ru>
2020-01-17 14:06:28 +0700

    17.01.20 + Пагинация

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import './main.css'
 
-import { createStore } from 'redux';
-import { createBrowserHistory } from 'history';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore, applyMiddleware} from 'redux'
+import {createBrowserHistory} from 'history'
+import thunk from 'redux-thunk'
+import {routerMiddleware, ConnectedRouter} from 'connected-react-router'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import {Provider} from 'react-redux'
+
+import createRootReducer from 'reducers'
+import Layout from 'containers/layout'
 
 const history = createBrowserHistory()
+const middlewares = [thunk, routerMiddleware(history)]
 const store = createStore(
-    createRootReduser(history)
+    createRootReducer(history),
+    composeWithDevTools(applyMiddleware(...middlewares))
 )
 
-document.write('Hello React/Redux!')
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <Layout />
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root')
+)
